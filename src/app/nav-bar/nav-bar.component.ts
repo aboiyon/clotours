9a3a24d6a6c81920942fb06e5255fc57e3728a5d
kiprotich <kiprotichmichael570@gmail.com>
2023-07-17 17:17:23 +0300

    refactor: use search icon for a search form in the nav bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
+
 export class NavBarComponent {
+
+  @ViewChild('searchIcon') searchIcon!: ElementRef;
+  @ViewChild('searchForm') searchForm!: ElementRef;
+
+  ngOnInit() {
+    this.searchIcon.nativeElement.addEventListener('click', () => {
+      this.searchForm.nativeElement.classList.toggle('d-none');
+    });
+  }
+
+  isSearchFormHidden:boolean = true;
+
+  toggleSearchForm(): void {
+    this.isSearchFormHidden = !this.isSearchFormHidden;
+  }
 
 }
