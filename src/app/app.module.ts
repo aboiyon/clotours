e1a303f4d08f06182dbd3e9a439d4b60cb4a50cc
kiprotich <kiprotichmichael570@gmail.com>
2023-07-17 17:41:14 +0300

    refactor: add route for services page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
