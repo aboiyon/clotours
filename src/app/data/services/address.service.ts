f8ff13f0bdf3125ee6e2139ea62dc137b483a8fe
kiprotich <kiprotichmichael570@gmail.com>
2023-09-05 15:37:50 +0300

    add address service

--- src/app/data/services/address.service.ts
+++ src/app/data/services/address.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Address } from '../models/address';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddressService {
 
-  constructor() { }
+  private url: string = `${environment.apiUrl}/api/addresses`;
+
+  constructor(private http: HttpClient, private eh: HttpErrorHandler) { }
+
+  createAddress(address: Address): Observable<Address> {
+    return this.http.post<Address>(this.url, address)
+      .pipe(catchError(this.eh.handleError));
+  }
+
+  getAddress(id: string): Observable<Address> {
+    return this.http.get<Address>(`${this.url}/${id}`)
+      .pipe(catchError(this.eh.handleError));
+  }
+  
 }
