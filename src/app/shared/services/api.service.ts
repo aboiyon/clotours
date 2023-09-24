faec41538e88f037a3f4443fd9aec86701269975
kiprotich <kiprotichmichael570@gmail.com>
2023-09-24 22:34:00 +0300

    add api service logic

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,23 +1,52 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/features/models/products.product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+
+  baseUrl = 'http://localhost:4567';
   
 
   constructor(private http: HttpClient) { }
+  
+  getAll():Observable<Product[]>{
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+  
+  get(id:any):Observable<any>{
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
 
-  // getItem(key: string): string | null {
-  //   const url = `/api/v1/items/${key}`;
-  //   const response = this.http.get(url);
+  create(data:any):Observable<any>{
+    return this.http.post(this.baseUrl,data);
+  }
 
-  //   if (response.status === 200) {
-  //     return response.json();
-  //   } else {
-  //     return null;
-  //   }
-  // }
+  update(id:number, data:any):Observable<any>{
+    return this.http.put(`${this.baseUrl}/${id}`,data);
+  }
+
+  delete(id:number):Observable<any>{
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+
+  deleteAll():Observable<any>{
+    return this.http.delete(this.baseUrl);
+  }
+
+  findByName(product_name:string):Observable<Product[]>{
+    return this.http.get<Product[]>(`${this.baseUrl}?name=${product_name}`);
+  }
+
+  getImage(id:number):Observable<any>{
+    return this.http.get(`${this.baseUrl}/${id}/image`);
+  }
+
+  postImage(id:number,data:any):Observable<any>{
+    return this.http.post(`${this.baseUrl}/${id}/image`,data);
+  }
   
 }
