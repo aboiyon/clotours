425daa26102d58a4774d64903d74ebf69f40fba8
kiprotich <kiprotichmichael570@gmail.com>
2023-11-09 17:37:12 +0300

    clean code up

--- src/app/features/products/products-routing.module.ts
+++ src/app/features/products/products-routing.module.ts
@@ -1,11 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SingleProductComponent } from './pages/single-product/single-product.component';
-import { ToursComponent } from './pages/tours/tours.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { ClothingComponent } from './pages/clothing/clothing/clothing.component';
-import { HomeMadeComponent } from './pages/clothing/home-made/home-made.component';
-import { ImportsComponent } from './pages/clothing/imports/imports.component';
 import { KidsComponent } from './pages/clothes/kids/kids.component';
 import { KidDetailComponent } from './pages/clothes/kid-detail/kid-detail.component';
 import { MenComponent } from './pages/clothes/men/men.component';
@@ -14,17 +8,6 @@ import { WomenComponent } from './pages/clothes/women/women.component';
 import { WomanDetailComponent } from './pages/clothes/woman-detail/woman-detail.component';
 
 const routes: Routes = [
-
-  {
-    path: 'products', component: ProductsComponent,
-  },
-
-  {
-    path: 'tours', component: ToursComponent 
-  },
-  {
-    path: 'tours/:id', component: SingleProductComponent
-  },
   
   {
     path: 'kids', component: KidsComponent
@@ -47,25 +30,6 @@ const routes: Routes = [
 
   {
     path: 'women/:id', component: WomanDetailComponent
-  },
-
-  {
-    path: 'clothing',
-    component: ClothingComponent,
-  },
-  {
-    path: 'home-made',
-    component:  HomeMadeComponent,
-  },
-  {
-    path: 'home-made/:id', component: SingleProductComponent
-  },
-  {
-    path: 'imports',
-    component:  ImportsComponent,
-  },
-  {
-    path: 'imports/:id', component: SingleProductComponent
   }
   
 ];
--- src/app/features/products/products.module.ts
+++ src/app/features/products/products.module.ts
@@ -5,13 +5,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 
 
 import { ProductsRoutingModule } from './products-routing.module';
-import { ProductsComponent } from './pages/products/products.component';
 import { ProductComponent } from './pages/product/product.component';
-import { ToursComponent } from './pages/tours/tours.component';
-import { SingleProductComponent } from './pages/single-product/single-product.component';
-import { ClothingComponent } from './pages/clothing/clothing/clothing.component';
-import { ImportsComponent } from './pages/clothing/imports/imports.component';
-import { HomeMadeComponent } from './pages/clothing/home-made/home-made.component';
 import { MenComponent } from './pages/clothes/men/men.component';
 import { WomenComponent } from './pages/clothes/women/women.component';
 import { KidsComponent } from './pages/clothes/kids/kids.component';
@@ -22,12 +16,6 @@ import { ManDetailComponent } from './pages/clothes/man-detail/man-detail.compon
 @NgModule({
   declarations: [
     ProductComponent,
-    ProductsComponent,
-    ToursComponent,
-    SingleProductComponent,
-    ClothingComponent,
-    ImportsComponent,
-    HomeMadeComponent,
     MenComponent,
     WomenComponent,
     KidsComponent,
@@ -42,11 +30,6 @@ import { ManDetailComponent } from './pages/clothes/man-detail/man-detail.compon
   ],
   exports: [
     ProductComponent,
-    ToursComponent,
-    SingleProductComponent,
-    ClothingComponent,
-    ImportsComponent,
-    HomeMadeComponent,
     KidsComponent,
     MenComponent,
     WomenComponent
