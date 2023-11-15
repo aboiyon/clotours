115ad2ca3e9c9f0464c3c5bf5c4ff44a5e2195ee
kiprotich <kiprotichmichael570@gmail.com>
2023-11-15 16:04:43 +0300

    add designer service

---
+++ src/app/features/services/designers.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/products.product';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DesignersService {
+  private url: string = `${this.apiService.baseUrl}/designers`;
+
+  constructor(private http: HttpClient, private apiService: ApiService) { }
+
+  getAll():Observable<Product[]>{
+    return this.http.get<Product[]>(this.url);
+  }
+  get(id:any):Observable<any>{
+    return this.http.get(`${this.url}/${id}`);
+  }
+  create(data:any):Observable<any>{
+    return this.http.post(this.url,data);
+  }
+  update(id:number, data:any):Observable<any>{
+    return this.http.put(`${this.url}/${id}`,data);
+  }
+  delete(id:number):Observable<any>{
+    return this.http.delete(`${this.url}/${id}`);
+  }
+  deleteAll():Observable<any>{
+    return this.http.delete(this.url);
+  }
+  findByName(name:string):Observable<Product[]>{
+    return this.http.get<Product[]>(`${this.url}?name=${name}`);
+  }
+  getImage(id:number):Observable<any>{
+    return this.http.get(`${this.url}/${id}/image`);
+  }
+  postImage(id:number,data:any):Observable<any>{
+    return this.http.post(`${this.url}/${id}/image`,data);
+  }
+}
