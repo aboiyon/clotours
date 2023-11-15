c98fc9718cff8e687c6981f32bf4d66088881d12
kiprotich <kiprotichmichael570@gmail.com>
2023-11-15 17:00:05 +0300

    optimize imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home-section/home/home.component';
-import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
 import { MainPageComponent } from './home-section/main-page/main-page.component';
 
 const routes: Routes = [
