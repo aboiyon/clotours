3b56ade1569510623f55a1b96ea2be2c69869f88
kiprotich <kiprotichmichael570@gmail.com>
2023-11-17 14:26:26 +0300

    add scroll function

--- src/app/features/products/pages/clothes/designer/designer.component.ts
+++ src/app/features/products/pages/clothes/designer/designer.component.ts
@@ -90,15 +90,45 @@ ngAfterViewInit(): void {
     })
   }
 
-scrollRight() {
-throw new Error('Method not implemented.');
-}
-navigateToClotheDetail(arg0: any) {
-throw new Error('Method not implemented.');
-}
-
 scrollLeft() {
-throw new Error('Method not implemented.');
-}
+    if (this.cardsWrapper) {
+      const scrollAmount = -400;
+      const currentScroll = this.cardsWrapper.nativeElement.scrollLeft;
+      this.scrollTo(this.cardsWrapper.nativeElement, currentScroll + scrollAmount, 400);
+    }
+  }
+
+  scrollRight() {
+    if (this.cardsWrapper) {
+      const scrollAmount = 400;
+      const currentScroll = this.cardsWrapper.nativeElement.scrollLeft;
+      this.scrollTo(this.cardsWrapper.nativeElement, currentScroll + scrollAmount, 400);
+    }
+  }
+
+  private scrollTo(element: HTMLElement, to: number, duration: number) {
+    const start = element.scrollLeft;
+    const change = to - start;
+    const increment = 20;
+    let currentTime = 0;
+
+    const animateScroll = () => {
+      currentTime += increment;
+      const val = this.easeInOutQuad(currentTime, start, change, duration);
+      element.scrollLeft = val;
+      if (currentTime < duration) {
+        setTimeout(animateScroll, increment);
+      }
+    };
+
+    animateScroll();
+  }
+
+  private easeInOutQuad(t: number, b: number, c: number, d: number): number {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
 
 }
--- src/app/features/products/pages/product/product.component.ts
+++ src/app/features/products/pages/product/product.component.ts
@@ -17,46 +17,5 @@ export class ProductComponent {
     imageUrl: '',
     price: 0
   };
-  
-  scrollLeft() {
-    if (this.cardsWrapper) {
-      const scrollAmount = -300;
-      const currentScroll = this.cardsWrapper.nativeElement.scrollLeft;
-      this.scrollTo(this.cardsWrapper.nativeElement, currentScroll + scrollAmount, 500);
-    }
-  }
-
-  scrollRight() {
-    if (this.cardsWrapper) {
-      const scrollAmount = 300;
-      const currentScroll = this.cardsWrapper.nativeElement.scrollLeft;
-      this.scrollTo(this.cardsWrapper.nativeElement, currentScroll + scrollAmount, 500);
-    }
-  }
-
-  private scrollTo(element: HTMLElement, to: number, duration: number) {
-    const start = element.scrollLeft;
-    const change = to - start;
-    const increment = 20;
-    let currentTime = 0;
-
-    const animateScroll = () => {
-      currentTime += increment;
-      const val = this.easeInOutQuad(currentTime, start, change, duration);
-      element.scrollLeft = val;
-      if (currentTime < duration) {
-        setTimeout(animateScroll, increment);
-      }
-    };
-
-    animateScroll();
-  }
-
-  private easeInOutQuad(t: number, b: number, c: number, d: number): number {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-  }
 
 }
